90d08c14f0d199b86190d244e863891973091502
Siyu Liu <siyuliu@unc.edu>
2026-04-12 21:34:13 -0400

    Initial Commit

---
+++ iOS_Final_Project/iOS_Final_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Final_ProjectApp.swift
+//  iOS_Final_Project
+//
+//  Created by Siyu Liu on 4/12/26.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Final_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
